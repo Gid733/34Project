f6cddfc3e6506cdc161ad3c52041fb489775adac
Gid <gid733@ya.ru>
2016-08-11 16:49:07 +0300

    change commit

--- Project34/ImageWorker.cs
+++ Project34/ImageWorker.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Net;
 
 namespace Project34
 {
-    class Program
+    class ImageWorker
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
--- Project34/RequestFilter.cs
+++ Project34/RequestFilter.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Net;
 
 namespace Project34
 {
-    class Program
+    class RequestFilter
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
